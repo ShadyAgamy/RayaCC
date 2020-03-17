cec13b518ea9c51846f3deb4f5ee97fe56170815
Shady Agami <sagamy@road9media.com>
2020-03-17 13:36:00 +0200

    inverstor result page and quick fixes

--- src/js/script.js
+++ src/js/script.js
@@ -329,6 +329,22 @@ function myFunction(checkMedia) {
   
     
   
+    //   accordion toggle
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        $(this).find("img").toggleClass("active")
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+    }
   
 
 
